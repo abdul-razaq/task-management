e494aec332a545f9503d4f3de87f27a8869e2529
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-02-08 16:12:42 +0100

    added service to create a new task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
+import { Task, TaskStatus } from './task.model';
+
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: Task[] = [];
 
-  public getAllTasks() {
+  public getAllTasks(): Task[] {
     return this.tasks;
   }
+
+  public createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: `task-${this.tasks.length + 1}`,
+      title,
+      description,
+      createdOn: new Date(Date.now()).toUTCString(),
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    // it is a good practice to return the newly created resource in a REST API.
+    return task;
+  };
 }
