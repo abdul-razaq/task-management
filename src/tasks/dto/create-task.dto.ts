c6b8c990ba7f7480056303874dd38d69bd704423
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-02-10 16:48:59 +0100

    added decorators from the class-validator package to add rules to the dto properties

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateTaskDto {
+  // these decorators add some rules to the properties of the DTO class.
+  @IsNotEmpty()
   title: string;
+  @IsNotEmpty()
   description: string;
 }
