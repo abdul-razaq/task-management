b612dccad110dfebe6da1b79ceda8a3bd338eb28
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-02-14 10:46:34 +0100

    about to use Repository to talk to the database instead of the Entity

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -4,6 +4,7 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Query,
@@ -13,43 +14,43 @@ import {
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
-import { Task, TaskStatus } from './task.model';
+import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
-  @Get()
-  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
-    if (Object.keys(filterDto).length) {
-      return this.tasksService.getTasksWithFilter(filterDto);
-    }
-    return this.tasksService.getAllTasks();
-  }
+  // @Get()
+  // getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
+  //   if (Object.keys(filterDto).length) {
+  //     return this.tasksService.getTasksWithFilter(filterDto);
+  //   }
+  //   return this.tasksService.getAllTasks();
+  // }
 
   @Get('/:id')
-  getTaskById(@Param('id') id: string): Task {
+  getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
     return this.tasksService.getTaskById(id);
   }
 
-  @Delete('/:id')
-  deleteTask(@Param('id') id: string): void {
-    this.tasksService.deleteTask(id);
-  }
+  // @Delete('/:id')
+  // deleteTask(@Param('id') id: string): void {
+  //   this.tasksService.deleteTask(id);
+  // }
 
-  @Patch('/:id/status')
-  updateTaskStatus(
-    @Param('id') id: string,
-    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-  ): Task | string {
-    if (!status) return 'status field is required.';
-    return this.tasksService.updateTaskStatus(id, status);
-  }
+  // @Patch('/:id/status')
+  // updateTaskStatus(
+  //   @Param('id') id: string,
+  //   @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  // ): Task | string {
+  //   if (!status) return 'status field is required.';
+  //   return this.tasksService.updateTaskStatus(id, status);
+  // }
 
   @Post()
   @UsePipes(ValidationPipe)
-  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+  createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return this.tasksService.createTask(createTaskDto);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,61 +1,76 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
-
-import { Task, TaskStatus } from './task.model';
+import { TaskStatus } from './task-status.enum';
+import { Task } from './task.entity';
+import { TaskRepository } from './task.repository';
 
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = [];
+  constructor(
+    @InjectRepository(TaskRepository)
+    private taskRepository: TaskRepository,
+  ) {}
+  // public getAllTasks(): Task[] {
+  //   return this.tasks;
+  // }
 
-  public getAllTasks(): Task[] {
-    return this.tasks;
-  }
+  // public getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
+  //   const { status, search } = filterDto;
+  //   let tasks = this.getAllTasks();
+  //   if (status) {
+  //     tasks = tasks.filter((task) => task.status === status);
+  //   }
+  //   if (search) {
+  //     tasks = tasks.filter(
+  //       (task) =>
+  //         task.title.includes(search) || task.description.includes(search),
+  //     );
+  //   }
+  //   return tasks;
+  // }
 
-  public getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
-    const { status, search } = filterDto;
-    let tasks = this.getAllTasks();
-    if (status) {
-      tasks = tasks.filter((task) => task.status === status);
-    }
-    if (search) {
-      tasks = tasks.filter(
-        (task) =>
-          task.title.includes(search) || task.description.includes(search),
-      );
-    }
-    return tasks;
-  }
-
-  public getTaskById(id: string): Task {
-    const foundTask = this.tasks.find((task) => task.id === id);
-    if (!foundTask)
+  public async getTaskById(id: number): Promise<Task> {
+    const task = await this.taskRepository.findOne(id);
+    if (!task)
       throw new NotFoundException(`task with the ID of ${id} not found.`);
-    return foundTask;
-  }
-
-  public deleteTask(id: string): void {
-    const taskToDelete = this.getTaskById(id);
-    this.tasks = this.tasks.filter((task) => task.id !== taskToDelete.id);
-  }
-
-  public updateTaskStatus(id: string, status: TaskStatus): Task | string {
-    const taskToUpdate = this.getTaskById(id);
-    taskToUpdate.status = status;
-    return taskToUpdate;
-  }
-
-  public createTask(createTaskDto: CreateTaskDto): Task {
-    const { title, description } = createTaskDto;
-    const task: Task = {
-      id: `task-${this.tasks.length + 1}`,
-      title,
-      description,
-      createdOn: new Date(Date.now()).toUTCString(),
-      status: TaskStatus.OPEN,
-    };
-    this.tasks.push(task);
-    // it is a good practice to return the newly created resource in a REST API.
     return task;
   }
+
+  // public deleteTask(id: string): void {
+  //   const taskToDelete = this.getTaskById(id);
+  //   this.tasks = this.tasks.filter((task) => task.id !== taskToDelete.id);
+  // }
+
+  // public updateTaskStatus(id: string, status: TaskStatus): Task | string {
+  //   const taskToUpdate = this.getTaskById(id);
+  //   taskToUpdate.status = status;
+  //   return taskToUpdate;
+  // }
+
+  public async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const { title, description } = createTaskDto;
+    const newTask: Task = new Task();
+    newTask.title = title;
+    newTask.description = description;
+    newTask.status = TaskStatus.OPEN;
+
+    await newTask.save();
+    return newTask;
+  }
+
+  // public createTask(createTaskDto: CreateTaskDto): Task {
+  //   const { title, description } = createTaskDto;
+  //   const task: Task = {
+  //     id: `task-${this.tasks.length + 1}`,
+  //     title,
+  //     description,
+  //     createdOn: new Date(Date.now()).toUTCString(),
+  //     status: TaskStatus.OPEN,
+  //   };
+  //   this.tasks.push(task);
+  //   // it is a good practice to return the newly created resource in a REST API.
+  //   return task;
+  // }
 }
